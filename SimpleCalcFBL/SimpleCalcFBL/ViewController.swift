3e777b21a5b7b323de9d7c593459dbbaf6ca789f
Alexander Makarov <fsbisan@mail.ru>
2022-09-05 17:37:45 +0500

    first comit

---
+++ SimpleCalcFBL/SimpleCalcFBL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCalcFBL
+//
+//  Created by Александр Макаров on 05.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
